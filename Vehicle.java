3a082b6ccbc0829623a987aa5d96f85830faeb95
GIRIVASAN M V <147172260+GIRIVASAN24@users.noreply.github.com>
2025-01-02 11:44:22 +0530

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,23 @@
+abstract class Vehicle{
+    private String id;
+    private boolean available;
+    public Vehicle(String id){
+        this.id=id;
+        this.available=true;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public boolean isAvailable() {
+        return available;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    
+    public abstract double getrentcost(int days);
+    public abstract double getmileage();
+}
